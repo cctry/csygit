9155e87458724e6626a7e6b5ae3caeaa2cbf0c9d
Shiyang <csycfl@gmail.com>
2018-06-04 12:24:23 +0800

    reconstruct utils file with namespace

--- utils.cpp
+++ utils.cpp
@@ -1,7 +1,7 @@
-#include "util.hpp"
+#include "utils.hpp"
 #include <sys/stat.h>
 using namespace std;
-int get_file_size(string path) {
+int util::get_file_size(string path) {
     const char* filename = path.c_str();
     struct stat statbuf;
     stat(filename, &statbuf);
@@ -13,8 +13,19 @@ int get_file_size(string path) {
 #include <iostream>
 #include <streambuf>
 #include <string>
-string get_file_content(string path) {
+string util::get_file_content(string path) {
     ifstream t(path.c_str());
     string str((istreambuf_iterator<char>(t)), istreambuf_iterator<char>());
+    t.close();
     return str;
+}
+
+#include <unistd.h>
+
+bool util::is_file_exist(string path) {
+    if (access(path.c_str(), F_OK) == -1) {
+        return false;
+    } else {
+        return true;
+    }
 }
--- utils.hpp
+++ utils.hpp
@@ -1,6 +1,9 @@
 #if !defined(UTIL_H)
 #define UTIL_H
 #include <string>
+namespace util {
 extern int get_file_size(std::string path);
 extern std::string get_file_content(std::string path);
-#endif // UTIL_H
+extern bool is_file_exist(std::string path);
+}  // namespace util
+#endif  // UTIL_H
