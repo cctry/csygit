4732c0524dacd1a5ebbc0e9d31a5fef7053306c0
Shiyang <csycfl@gmail.com>
2018-06-10 21:44:51 +0800

    ad a new util function that is the same as the 'ls' command

--- utils.cpp
+++ utils.cpp
@@ -1,5 +1,6 @@
 #include "utils.hpp"
 #include <sys/stat.h>
+#include "cerror.hpp"
 using namespace std;
 int util::get_file_size(string path) {
     const char* filename = path.c_str();
@@ -21,11 +22,51 @@ string util::get_file_content(string path) {
 }
 
 #include <unistd.h>
-
 bool util::is_file_exist(string path) {
     if (access(path.c_str(), F_OK) == -1) {
         return false;
     } else {
         return true;
     }
+}
+
+#include <dirent.h>
+#define FILE_MODE 8
+#define DIR_MODE 4
+#define ALL_MODE -1
+
+vector<string> scan_dir(int mode, string dirname) {
+    DIR* dp;
+    struct dirent* dirp;
+    if ((dp = opendir(dirname.c_str())) == NULL)
+        cerror::occur_error("Cannot open " + dirname);
+        //cout << "Cannot open " << dirname << endl;
+    vector<string> res;
+    while ((dirp = readdir(dp)) != NULL) {
+        switch (mode) {
+            case FILE_MODE:
+                if (dirp->d_type == 8)
+                    res.push_back(string(dirp->d_name));
+                break;
+            case DIR_MODE:
+                if (dirp->d_type == 4) {
+                    if (string(dirp->d_name) != ".." &&
+                        string(dirp->d_name) != ".") {
+                        res.push_back(string(dirp->d_name));
+                    }
+                }
+                break;
+            default:
+                res.push_back(string(dirp->d_name));
+        }
+    }
+    return res;
+}
+
+vector<string> util::get_all_files(string path) {
+    return scan_dir(FILE_MODE, path);
+}
+
+vector<string> util::get_all_dirs(string path) {
+    return scan_dir(DIR_MODE, path);
 }
--- utils.hpp
+++ utils.hpp
@@ -1,9 +1,12 @@
 #if !defined(UTIL_H)
 #define UTIL_H
 #include <string>
+#include <vector>
 namespace util {
 extern int get_file_size(std::string path);
 extern std::string get_file_content(std::string path);
 extern bool is_file_exist(std::string path);
+extern std::vector<std::string> get_all_files(std::string path);
+extern std::vector<std::string> get_all_dirs(std::string path);
 }  // namespace util
 #endif  // UTIL_H
